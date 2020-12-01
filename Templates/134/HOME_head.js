467dfa650f35d88ac47dc57ea9e9598f1cc6cd89
Adnan <73897123+githubbyad@users.noreply.github.com>
2020-12-01 19:19:41 +0530

    Create HOME_head.js

---
+++ Templates/134/HOME_head.js
@@ -0,0 +1,4 @@
+<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100&display=swap" rel="stylesheet">
+<script async src="https://static.addtoany.com/menu/page.js"></script>
