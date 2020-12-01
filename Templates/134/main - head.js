8d49f64ba045f592cf95d2e3aca7442bcfe72cae
Adnan <73897123+githubbyad@users.noreply.github.com>
2020-12-01 19:20:41 +0530

    Create main - head.js

---
+++ Templates/134/main - head.js
@@ -0,0 +1,2 @@
+<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
