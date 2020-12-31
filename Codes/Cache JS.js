804c4d1018160b0ab7c59dbb331d6195c4bce4dd
Adnan <73897123+githubbyad@users.noreply.github.com>
2020-12-31 23:05:16 +0530

    Update Cache JS.js

--- Codes/Cache JS.js
+++ Codes/Cache JS.js
@@ -24,6 +24,9 @@ for (var i = 0; i < links.length; i++) {
     if (scrpt.src.indexOf('custom.js?') != -1) {
         scrpt.src += "v=" + vl;
     }
+    if (scrpt.src.indexOf('{{PutTFolder}}.js?') != -1) {
+        scrpt.src += "v=" + vl;
+    }
 }
 }
 </script>
